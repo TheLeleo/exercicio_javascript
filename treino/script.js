8ab5601f04262f1241bf15ce650068045885ffe3
TheLeleo <99210769+TheLeleo@users.noreply.github.com>
2022-05-05 13:55:46 -0300

    Add files via upload

--- treino/script.js
+++ treino/script.js
@@ -5,8 +5,14 @@ function ex1(){
  const g = 10; // aceleração gravitacional 10m/s^2
 
  tSubida = v0/g;
- document.getElementById('vem').innerHTML = "Tempo de subida = " + tSubida + " m/s²"; // Imprimir no p
- console.log("TEMPO DE SUBIDA = " + tSubida); // imprimir no console
- alert("TEMPO DE SUBIDA " + tSubida); // imprimir no alert
+ alturaM = ((v0*v0)/(g*2));
+
+ document.getElementById('vem').innerHTML = "Tempo de subida = " + alturaM + " m/s²"; // Imprimir no p
+ console.log("TEMPO DE SUBIDA = " + tSubida + " m/s²"); // imprimir no console
+ alert("TEMPO DE SUBIDA = " + tSubida + " m/s²"); // imprimir no alert
+
+ document.getElementById('altura').innerHTML = "Altura Máxima = " + alturaM + "m";
+ console.log("ALTURA MÁXIMA = " + alturaM + "m");
+ alert("ALTURA MÁXIMA = " + alturaM + "m");
 
 }
